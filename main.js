5b8e9244159c932f730e72a5b79f322f32dcffcd
Font9876 <hollholl1@hotmail.com>
2025-06-15 12:30:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const log = document.getElementById('log');
 document.getElementById('connect').addEventListener('click', async () => {
   try {
-    const device = await navigator.usb.requestDevice({ filters: [{vendorId: 0x239A}] });
+    const device = await navigator.usb.requestDevice({ filters: [{vendorId: 0x000A}] });
     await device.open();
     if (!device.configuration) await device.selectConfiguration(1);
     await device.claimInterface(0);
